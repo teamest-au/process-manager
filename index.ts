90a9a6fbb461a6dad33c8435faba17fa89afe21c
Daniel Emery <danielremery@gmail.com>
2020-11-07 16:55:22 +1030

    Added missing export for IServiceStatus

--- index.ts
+++ index.ts
@@ -4,4 +4,5 @@ export { default as IProcessManagerOptions } from './src/IProcessManagerOptions'
 export { default as IProcessManagerService } from './src/IProcessManagerService';
 export { default as IProcessStatus } from './src/IProcessStatus';
 export { default as IServiceHealth } from './src/IServiceHealth';
+export { default as IServiceStatus } from './src/IServiceStatus';
 export { default as ProcessManager } from './src/ProcessManager';
